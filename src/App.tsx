520ab2f4deb9809b59c708b7fc338a649869de74
aminetouaoula <touaoulamohamedamine01@gmail.com>
2023-10-24 11:09:24 +0100

    v0.0.1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// import { Button } from "react-bootstrap";
+import NavBar from "./Components/NavBar";
+import "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <NavBar></NavBar>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/NavBar.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function NavBar() {
+  return (
+    <>
+      <div className="navbar navbar-expand navbar-dark">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            Dollar
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default NavBar;
